f3dc7729a67f65b1915274c4c0fc3f6c5b4e3e56
Axel <axelsorensenwork@gmail.com>
2024-11-03 03:08:28 +0100

    changed link for buymecoffee

--- components/Support.vue
+++ components/Support.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex items-center h-10 text-white flex-col gap-2 justify-center">
 
-        <NuxtLink to="https://buymeacoffee.com/turingtalks"
+        <NuxtLink to="https://buymeacoffee.com/theturingtalks"
             class="whitespace-nowrap hover:brightness-110 hover:scale-105 transition-all gap-2 flex p-3  px-8 rounded-lg  bg-[#5F7FFF]">
             <Coffee class="h-6 w-6" />
             <p class="font-medium">Buy us a coffee</p>
